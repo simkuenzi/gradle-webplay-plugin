1e0a3c83f8a932ee617cac09c0bcb2817364c27f
Simon Künzi <sim.kuenzi@gmail.com>
2020-07-31 14:26:09 +0200

    Remove diagnostic printlns from tests

--- src/test/java/com/github/simkuenzi/webplay/WebplayGradlePluginTest.java
+++ src/test/java/com/github/simkuenzi/webplay/WebplayGradlePluginTest.java
@@ -54,7 +54,6 @@ public class WebplayGradlePluginTest {
         try {
             int maxStopTry = 100;
             for (int stopTry = 0; stopTry < maxStopTry; stopTry++) {
-                System.out.println("Try stop");
                 GradleRunner.create()
                         .forwardOutput()
                         .withProjectDir(testProjectDir.getRoot())
@@ -64,22 +63,17 @@ public class WebplayGradlePluginTest {
                         .getTasks().forEach(t -> assertEquals(TaskOutcome.SUCCESS, t.getOutcome()));
                 recordThread.join(200);
                 if (!recordThread.isAlive()) {
-                    System.out.println("Stopped");
                     break;
                 }
             }
             assertFalse("Stopping of recorder thread takes too long.", recordThread.isAlive());
         } finally {
-            System.out.println("Finally");
             if (recordThread.isAlive()) {
-                System.out.println("Force stop");
                 recordThread.interrupt();
                 recordThread.join();
-                System.out.println("Force stop complete");
             }
         }
 
-        System.out.println("Test end");
         assertNull("Exception thrown by recorder thread.", recordException[0]);
         recordTasks.forEach(t -> assertEquals(TaskOutcome.SUCCESS, t.getOutcome()));
     }
